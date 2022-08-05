72ce1dd00c07d41c0bb865f43a631e88f753e62f
lotusCyrus <johnsonsolotu@gmail.com>
2022-08-05 06:24:19 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+ <h1 ref="name">{{title}}</h1>
+ <button @click="handleClick" >click me</button>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+      return{
+          title:'this page title'
+      }
+   
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
