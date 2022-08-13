87889e960d91404101cfe90cb8b7ff2758b09028
lotusCyrus <johnsonsolotu@gmail.com>
2022-08-13 16:39:40 +0100

    file updated

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
  <h1 ref="name">{{title}}</h1>
- <button @click="handleClick" >click me</button>
+ 
+<div v-if="showModal">
+ <Modal :header="header" :text="text" :theme="theme" @close="toggleModal">
+     <p>what a cool app</p>
+     <template v-slot:links> 
+        <a href="#">Signup</a>
+         <a href="#">More info</a>
+     </template>
+ </Modal>
+</div>
+<button @click="toggleModal">click me</button>
+
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
 export default {
   name: 'App',
+  components: {Modal},
   data(){
       return{
-          title:'this page title'
+          title:'this page title',
+          header:'This header of this modal',
+          text:'This is the page text',
+          theme:'sales',
+          showModal:false
       }
-   
-  }
+    },
+      
+      methods:{
+        toggleModal()
+        {
+            this.showModal = !this.showModal
+        }
+      }
+  
 }
 </script>
 
